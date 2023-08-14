133e8c614a37a728c46672bd328d6c3283723066
Jim Kroon <info@jimkroon.nl>
2023-08-14 17:57:15 +0200

    Update readme and tests

--- tests/xalauthenticator.js
+++ tests/xalauthenticator.js
@@ -5,7 +5,22 @@ const XalLibrary = new require('../dist/lib').default
 const xal = new XalLibrary.XalAuthenticator()
 
 test('XalAuthenticator should have an handler returned', (t) => {
-    assert.notDeepStrictEqual(xal.handler, undefined, "xal.handler should not be undefined");
+    assert.notDeepStrictEqual(xal.handler, undefined, "xal.handler should not be undefined")
+});
+
+test('XalAuthenticator should retrieve a deviceToken', (t) => {
+    const xal2 = new XalLibrary.XalAuthenticator()
+
+    xal2.get_device_token().then((device_token) => {
+        console.log('RESULT:', device_token)
+        assert.notDeepStrictEqual(device_token.Token, undefined, "Device token should not be undefined")
+        assert.notDeepStrictEqual(device_token.DisplayClaims, undefined, "DisplayClaims should not be undefined")
+
+        xal2.close()
+    }).catch((error) => {
+        console.log(error)
+        assert.fail('Failed to retrieve deviceTokens')
+    })
 });
 
 xal.close()
