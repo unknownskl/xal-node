a1b42a830c72028d85babe27a2e252e2a37fb169
Jim Kroon <info@jimkroon.nl>
2024-02-22 13:33:44 +0100

    Add test for state

--- tests/xalauthenticator.js
+++ tests/xalauthenticator.js
@@ -30,41 +30,7 @@ test('XalAuthenticator should retrieve a authentication URL', async (t) => {
     // console.log('sisuResponse', sisuResponse, codeChallange)
 
     assert.notDeepStrictEqual(codeChallange.verifier, undefined, "verifier should not be undefined")
+    assert.notDeepStrictEqual(state, undefined, "state should not be undefined")
     assert.notDeepStrictEqual(sisuResponse.SessionId, undefined, "SessionId should not be undefined")
     assert.notDeepStrictEqual(sisuResponse.MsaOauthRedirect, undefined, "MsaOauthRedirect should not be undefined")
 });
-
-// test('XalAuthenticator should retrieve a authentication URL', (t) => {
-//     const xal2 = new XalLibrary.XalAuthenticator()
-
-//     xal2.get_device_token().then((device_token) => {   
-//         // console.log('RESULT:', device_token)
-//         assert.notDeepStrictEqual(device_token.Token, undefined, "Device token should not be undefined")
-//         assert.notDeepStrictEqual(device_token.DisplayClaims, undefined, "DisplayClaims should not be undefined")
-
-//         xal2.do_sisu_authentication(device_token.Token).then((sisu_response) => {
-//             const _authFlowTokens = {
-//                 sisu_local_code_verifier: sisu_response.local_code_verifier,
-//                 sisu_session_id: sisu_response.sisu_session_id,
-//                 sisu_device_token: device_token,
-//                 redirect_uri: sisu_response.msal_response.MsaOauthRedirect,
-//             }
-//             assert.notDeepStrictEqual(sisu_response.local_code_verifier, undefined, "local_code_verifier should not be undefined")
-//             assert.notDeepStrictEqual(sisu_response.sisu_session_id, undefined, "local_code_verifier should not be undefined")
-//             assert.notDeepStrictEqual(sisu_response.msal_response.SessionId, undefined, "msal_response.MsaOauthRedirect should not be undefined")
-//             assert.notDeepStrictEqual(sisu_response.msal_response.MsaOauthRedirect, undefined, "msal_response.MsaOauthRedirect should not be undefined")
-
-//             // console.log('RESULT:', sisu_response)
-//             t()
-
-//         }).catch((error) => {
-//             assert.fail('do_sisu_authentication should not fail')
-//         })
-        
-//     }).catch((error) => {
-//         console.log(error)
-//         assert.fail('Failed to retrieve deviceTokens')
-//     })
-// });
-
-// xal.close()
